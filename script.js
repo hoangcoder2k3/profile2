96cbbf1e27934ea269f5c9a3e70fd78abca22c7b
Dang Van Hoang <100982457+hoangcoder2k3@users.noreply.github.com>
2024-05-02 22:19:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const tabs = document.querySelectorAll('.list-style.nav');
+const contents = document.querySelectorAll('.container-wrapper');
+const mains = document.querySelectorAll('.main');
+const closeButtons = document.querySelectorAll('.control-button button');
+const closeContactZalo = document.querySelector('.contact-zalo button');
+
+tabs.forEach((tab, index) => {
+    const content = contents[index];
+    tab.onclick = function () {
+        const activeTab = document.querySelector('.list-style.active');
+        const activeContent = document.querySelector('.container-wrapper.active');
+        if (activeTab) {
+            activeTab.classList.remove('active');
+        }
+        if (activeContent) {
+            activeContent.classList.remove('active');
+        }
+        this.classList.add('active');
+        content.classList.add('active');
+        console.log(index)
+
+
+        document.querySelector('.container-wrapper-first').style.animation = 'slideRight 0.5s ease-in-out forwards';
+        // setTimeout(() => {
+        //         const widthItem = document.querySelector('.container-wrapper-first').offsetWidth;
+        //         document.getElementById('scroll').scrollLeft += widthItem;
+        //     }, 250);
+    }
+});
+
+closeButtons.forEach(button => {
+    button.onclick = function () {
+        document.querySelector('.container-wrapper-first').style.animation = 'slideLeft 0.5s ease-in-out';
+
+        // const widthItem = document.querySelector('.container-wrapper-first').offsetWidth;
+        // document.getElementById('scroll').scrollLeft -= widthItem;
+        // setTimeout(() => {
+        // }, 250);
+    }
+})
+
+document.getElementById('openContactZalo').onclick = function(){
+    document.querySelector('.contact-zalo').classList.add('active');
+}
+closeContactZalo.onclick = function(){
+    document.querySelector('.contact-zalo').classList.remove('active');
+}
+document.querySelector('.contact-zalo').onclick = function(){
+    document.querySelector('.contact-zalo').classList.remove('active');
+}
